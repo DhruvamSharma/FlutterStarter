0856319d9ef057cc21c436a3ad111b46247dcf49
Dhruvam <dhruvamssharma@gmail.com>
2018-12-15 17:31:09 +0530

    Task Master: Added List Widget and completed the Codelab 1

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,70 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-          backgroundColor: Colors.amber,
-        ),
-        body: Center(
-          child: Text(WordPair("hello", "bye").asPascalCase),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
+}
+
+
+class RandomWords extends StatefulWidget {
+
+  @override
+  RandomWordState createState() => new RandomWordState();
+
+}
+
+class RandomWordState extends State<RandomWords> {
+
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    // returns a word pair for the list
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("MyTitle"),
+        backgroundColor: Colors.amber,
+      ),
+      body: _buildSuggestions(),
+
+    );
+
+  }
+
+  Widget _buildSuggestions() {
+    
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+
+          if(i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+
+          if(index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_suggestions[index]);
+        });
+    
+  }
+
+
+  Widget _buildRow(WordPair pair) {
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+      ),
+    );
+
+  }
+
+
+
 }
