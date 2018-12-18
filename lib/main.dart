77d1e1556f58ba34f7ecfead9719d34ecb9dffde
Dhruvam <dhruvamssharma@gmail.com>
2018-12-18 22:57:10 +0530

    Task Master: Completed Hello Rectangle App

--- lib/main.dart
+++ lib/main.dart
@@ -2,64 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(MyApp());
-  print("myData");
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          title: Text("hello Rectangle"),
+        ),
+        body: bodyWidget(),
+      )));
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: HelloRectangle(),
-    );
-  }
+Widget bodyWidget() {
+  return Center(
+    child: Container(
+      color: Colors.pink,
+      width: 300,
+      height: 300,
+      child: Center(
+        child: Text(
+          "MyText",
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+    ),
+  );
 }
-
 
 class HelloRectangle extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final _container = Container(
       color: Colors.cyan,
       margin: EdgeInsets.all(16.0),
       child: Center(
         child: Column(
-          children : <Widget>[
+          children: <Widget>[
             Text("Hello"),
             Text("Bye"),
           ],
         ),
       ),
-
     );
 
-    return Scaffold (
-      appBar: AppBar(
-        title: Text("Title"),
-        backgroundColor: Colors.amber,
-      ),
-      body : Center(
-        child: _container,
-      )
-    );
-
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Title"),
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: _container,
+        ));
   }
-
 }
 
-
 class RandomWords extends StatefulWidget {
-
   @override
   RandomWordState createState() => new RandomWordState();
-
 }
 
 class RandomWordState extends State<RandomWords> {
-
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
@@ -68,47 +70,35 @@ class RandomWordState extends State<RandomWords> {
     // returns a word pair for the list
 
     return Scaffold(
-
       appBar: AppBar(
         title: Text("MyTitle"),
         backgroundColor: Colors.amber,
       ),
       body: _buildSuggestions(),
-
     );
-
   }
 
   Widget _buildSuggestions() {
-    
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
-
-          if(i.isOdd) return Divider();
+          if (i.isOdd) return Divider();
 
           final index = i ~/ 2;
 
-          if(index >= _suggestions.length) {
+          if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
 
           return _buildRow(_suggestions[index]);
         });
-    
   }
 
-
   Widget _buildRow(WordPair pair) {
-
     return ListTile(
       title: Text(
         pair.asPascalCase,
       ),
     );
-
   }
-
-
-
 }
